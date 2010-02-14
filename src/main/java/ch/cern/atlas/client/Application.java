8631f045b63d1697a0d50155e79daad03991399d
Mark Donszelmann <duns@macserver.donszelmann.org>
2010-02-14 21:47:00 +0100

    Initial trial

---
+++ src/main/java/ch/cern/atlas/client/Application.java
@@ -0,0 +1,18 @@
+package ch.cern.atlas.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Application implements EntryPoint {
+
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+		HelloWorldExample hello = new HelloWorldExample();
+		RootPanel.get().add(hello);
+	}
+}
